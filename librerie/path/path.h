59e6d371b33a859c9cfef0aeddffba779d0a9e3c
Sebastiano Pagano <sebastiano.pagano.lcm@gmail.com>
2019-10-22 16:42:45 +0200

    prova

--- librerie/path/path.h
+++ librerie/path/path.h
@@ -11,21 +11,37 @@ class path {
     __device__ __host__ path();
     __device__ __host__ path(input_option_data o, input_market_data m);
     __device__ __host__ ~path(); //destructor
-    __device__ __host__ path(const path &path2);
+    // __device__ __host__ path(const path &path2);
 
 
     __device__ __host__ double eulero(double gauss);
-    // __device__ __host__ double payoff();
+    __device__ __host__ double payoff();
     // __device__ __host__ double Get_new_price();
     // double black_and_scholes();
   private:
 
-    double risk_free_rate;
-    double m_p;
-    double sigma;
-    double dt;
-    int    m_n_steps;
-    double m_E; //strike price, non so se considerarlo costante, o da passare al costruttore
+    //dati mercato
+
+    double m_risk_free_rate;
+    double m_prezzo_iniziale;
+    double m_volatility;
+
+    double step_price //il prezzo dummy su cui fare eulero
+
+    // dati opzione
+
+    char   m_option_type;
+    int    m_numero_steps;
+    double delta_time;
+    double m_time_to_maturity;
+    double m_strike_price;
+
+    //opzione corridor
+
+    double m_B;
+    double m_K;
+    double m_N; //Nozionale
+
 
 };
 
