953f011e94024642be608ac83ff90c0e549bb020
Sebastiano Pagano <sebastiano.pagano.lcm@gmail.com>
2020-03-27 14:55:08 +0100

    test output vector insied global caller

--- librerie/main.cu
+++ librerie/main.cu
@@ -21,14 +21,14 @@ int main() {
 
   string input_file("./data_manager/input_file.dat");
 //  string output_file_cpu("./data_manager/output_file_cpu.dat");
-  string output_file_gpu("./data_manager/output_file_gpu.dat");
+  // string output_file_gpu("./data_manager/output_file_gpu.dat");
 
   resultsManager rsm;
 
   rsm.Set_structs_value(market_data, option_data, mc_data, gpu_data, input_file);
 
   //output_statistica *output_cpu = new output_statistica[gpu_data.numero_thread_totali];
-  output_statistica *output_gpu = new output_statistica[gpu_data.numero_thread_totali];
+  // output_statistica *output_gpu = new output_statistica[gpu_data.numero_thread_totali];
 
   //std::cout << "begin CPU simulation ...." << '\n';
   //std::cout << " " << '\n';
@@ -42,7 +42,7 @@ int main() {
   std::cout << "begin GPU simulation ...." << '\n';
   std::cout << " " << '\n';
 
-  global_caller(market_data, option_data, mc_data, gpu_data, output_gpu);
+  global_caller(market_data, option_data, mc_data, gpu_data /*, output_gpu*/ );
 
   std::cout << " " << '\n';
   std::cout << "GPU simulation ended ...." << '\n';
@@ -58,10 +58,10 @@ int main() {
 */
 
 //  rsm.Print(output_file_cpu, output_cpu, gpu_data);
-  rsm.Print(output_file_gpu, output_gpu, gpu_data);
+  // rsm.Print(output_file_gpu, output_gpu, gpu_data);
 
 //  delete[] output_cpu;
-  delete[] output_gpu;
+  // delete[] output_gpu;
 
   return 0;
 
